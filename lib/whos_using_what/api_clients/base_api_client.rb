08ead99193625473b6da437646b0675814ffa2a3
devs1010 <r.dane1010@gmail.com>
2012-12-25 03:48:32 -0800

    update

--- lib/whos_using_what/api_clients/base_api_client.rb
+++ lib/whos_using_what/api_clients/base_api_client.rb
@@ -2,6 +2,57 @@ require_relative "../base"
 
 class BaseApiClient < Base
 
+  require "uri"
+  require "rest-client"
+
+  def arraySearch(array, rawHtml)
+
+    rawHtml = rawHtml.downcase
+    array.each do |token|
+      if (rawHtml.index(token) != nil)
+        return true
+      end
+    end
+    return false
+  end
+
+  def arry_to_str_delim array, delim
+
+    str = ""
+    i = 0
+    array.each do |entry|
+      if i < 1
+        str = entry.strip
+
+      else
+        str = str << delim << entry.strip
+      end
+      i += 1
+    end
+
+    str.strip
+  end
+
+
+  def cleanup_url url
+    #clean up url
+    url = url.strip
+    if url["www."] != nil
+      url["www."] = ""
+    end
+    url
+
+  end
+
+  def determineIfUsesTechnology(technology, rawHtml)
+
+    isJobPage = arraySearch(@jobPageTokens, rawHtml)
+
+    return isJobPage
+
+  end
+
+
   def starts_with?(string, prefix)
     prefix = prefix.to_s
     string[0, prefix.length] == prefix
